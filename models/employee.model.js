e0343e1af33fcd17ea283b347e8846aa81e643b0
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-09 22:56:32 +0530

    create employee model

---
+++ models/employee.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const employeeSchema = new mongoose.Schema(
+    {
+        firstName: String,
+        lastName: String,
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        rating: Number,
+        wage: Number,
+        experience: Number,
+        skills: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "JobType",
+            },
+        ],
+        completedJobs: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Job",
+            },
+        ],
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model("Employee", employeeSchema);
