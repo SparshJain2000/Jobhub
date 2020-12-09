89c708c45b7f25469e5a200f021374f132ecb088
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-10 01:49:55 +0530

    update graphql schema

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -6,15 +6,22 @@ module.exports = buildSchema(`
                 lastName:String!
                 email:String!
                 password:String!
+                experience:Int!
+                wage:Float!
+                skills:[JobType!]!
                 rating:Float!
                 createdAt:String!
                 updatedAt:String!
+            }
+            type JobType{
+                title:String!
             }
             type Employer {
                 _id:ID!
                 firstName:String!
                 lastName:String!
                 email:String!
+                jobs:[Job!]
                 password:String!
                 createdAt:String!
                 updatedAt:String!
@@ -23,13 +30,43 @@ module.exports = buildSchema(`
                 _id: ID!
                 title: String!
                 description: String!
-                type:[String]!
+                type:[JobType]!
                 date: String!
+                location:Location
                 creator: Employer!
             }   
+            type Location{
+                city:String
+                state:String
+                country:String
+                streetNo:String
+                pin:Int
+                Latitude:Float
+                Longitude:Float
+            }
+            input JobQuery{
+                type:String
+                lastDate:String
+                startDate:String
+                location:InputLocation
+            }
+            input InputLocation{
+                city:String
+                state:String
+                country:String
+                streetNo:String
+                pin:Int
+                Latitude:Float
+                Longitude:Float
+            }
             type RootQuery{
                 jobs: [Job!]!
+                job(id:ID!): Job!
+                searchJobs(params:JobQuery!): [Job!]
+                employers:[Employer!]!
+                employees:[Employee!]!
             }    
+            
             schema{
                     query: RootQuery
                 }
