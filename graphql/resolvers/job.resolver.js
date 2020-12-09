ecf5e48961b1a7bed3a93bda2e185228105b24e0
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-10 01:50:17 +0530

    job resolver populate and searchJobs

--- graphql/resolvers/job.resolver.js
+++ graphql/resolvers/job.resolver.js
@@ -3,7 +3,11 @@ const { transformJob } = require("../helpers/index");
 module.exports = {
     jobs: async () => {
         try {
-            const jobs = await Job.find().lean();
+            const jobs = await Job.find()
+                .populate("creator")
+                .populate("type")
+                .lean();
+
             return jobs.map((job) => transformJob(job));
         } catch (e) {
             console.log(e);
@@ -12,8 +16,30 @@ module.exports = {
     },
     job: async ({ id }) => {
         try {
-            const job = await Job.findById(id).lean();
-            return job;
+            const job = await Job.findById(id)
+                .populate("creator")
+                .populate("type")
+                .lean();
+            return transformJob(job);
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    },
+    searchJobs: async ({ params }) => {
+        console.log(params.location.city);
+        try {
+            const jobs = await Job.find({
+                "location.city": params.location.city,
+                date: {
+                    $gte: new Date(params.startDate),
+                    $lte: new Date(params.lastDate),
+                },
+            })
+                .populate("creator")
+                .populate("type")
+                .lean();
+            return jobs.map((job) => transformJob(job));
         } catch (e) {
             console.log(e);
             throw e;
