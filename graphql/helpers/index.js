8489daae57c9fb3ea2fad45ae8d13cee76be4a68
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-15 02:28:44 +0530

    add helpers : populateJobs,getJob

--- graphql/helpers/index.js
+++ graphql/helpers/index.js
@@ -1,7 +1,27 @@
 const Employer = require("../../models/employer.model");
-const transformJob = async (job) => {
-    const employer = await Employer.findById(job.creator).lean();
-    job = { ...job, creator: employer };
-    return job;
+const JobType = require("../../models/jobType.model");
+const Job = require("../../models/job.model");
+const transformJob = async(job) => {
+    try {
+        job = {...job, date: job.date.toISOString() };
+        return job;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
 };
-module.exports = { transformJob };
+const getJob = async(id) => {
+    return await Job.findById(id).lean();
+};
+const populateJobs = async(employer) => {
+    try {
+        return {
+            ...employer,
+            jobs: employer.jobs.map((job) => getJob(job)),
+        };
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
+module.exports = { transformJob, populateJobs };
