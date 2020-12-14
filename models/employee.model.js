f87daa952e699cf010207de007651e2007c0bcaf
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-15 02:27:48 +0530

    update employee model

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,27 +1,23 @@
 const mongoose = require("mongoose");
-const employeeSchema = new mongoose.Schema(
-    {
-        firstName: String,
-        lastName: String,
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        rating: Number,
-        wage: Number,
-        experience: Number,
-        skills: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "JobType",
-            },
-        ],
-        completedJobs: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Job",
-            },
-        ],
-    },
-    { timestamps: true },
-);
+const employeeSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    rating: Number,
+    wage: Number,
+    experience: Number,
+    skills: [{
+        typeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "JobType",
+        },
+        title: String,
+    }, ],
+    completedJobs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+    }, ],
+}, { timestamps: true }, );
 
 module.exports = mongoose.model("Employee", employeeSchema);
