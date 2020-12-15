56f67d9062f3028d450b3a8f794e969902565adf
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-16 00:57:18 +0530

    search employee resolver

--- graphql/resolvers/employee.resolver.js
+++ graphql/resolvers/employee.resolver.js
@@ -1,7 +1,26 @@
 const Employee = require("../../models/employee.model");
+const {
+    jobQuery,
+    projectionBuilder,
+    employeeQuery,
+} = require("../../controller/queryBuilder");
 module.exports = {
-    employees: async () => {
+    employees: async() => {
         const employees = await Employee.find().lean();
         return employees;
+    },
+    searchEmployees: async({ params }, args, context) => {
+        try {
+            const projection = projectionBuilder(context);
+            const employees = projection.completedJobs ?
+                await Employee.find(employeeQuery(params), projection)
+                .populate("completedJobs")
+                .lean() :
+                await Employee.find(employeeQuery(params), projection).lean();
+            return employees;
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
     },
 };
