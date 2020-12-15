def4b46f8fc192f3ba2ef517171c5f6ed6982e74
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-16 00:56:12 +0530

    search employee queryBuilder

--- controller/queryBuilder.js
+++ controller/queryBuilder.js
@@ -11,6 +11,23 @@ const jobQuery = (params) => {
         query.date = {...query.date, $lte: new Date(params.lastDate) };
     return query;
 };
+const employeeQuery = (params) => {
+    let query = {};
+    if (params.rating !== undefined) query.rating = { $gte: params.rating };
+    if (params.wage !== undefined) query.wage = { $lte: params.wage };
+    if (params.experience !== undefined)
+        query.experience = { $gte: params.experience };
+    if (params.jobCount !== undefined)
+        query["$expr"] = {
+            $gte: [{ $size: "$completedJobs" }, params.jobCount],
+        };
+    if (params.skills !== undefined) query["skills.title"] = params.skills;
+    if (params.location) {
+        const { location } = params;
+        for (const x in location) query[`location.${x}`] = location[x];
+    }
+    return query;
+};
 const projectionBuilder = (context) => {
     const projection = {};
     Object.keys(graphqlFields(context)).forEach(
@@ -18,4 +35,4 @@ const projectionBuilder = (context) => {
     );
     return projection;
 };
-module.exports = { jobQuery, projectionBuilder };
+module.exports = { jobQuery, projectionBuilder, employeeQuery };
