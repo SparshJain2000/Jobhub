4110fa0c0d492e02d6c9619d29badd6b92aa7091
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-21 02:08:33 +0530

    queryInput : save query and pass to props

--- client/src/components/queryInput.component.jsx
+++ client/src/components/queryInput.component.jsx
@@ -15,15 +15,51 @@ export default class QueryInput extends Component {
         super(props);
         this.state = {
             headers: { type: true },
-            query: {},
+            query: {
+                set: false,
+                type: {},
+            },
         };
         this.toggleHeaders = this.toggleHeaders.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.submitQuery = this.submitQuery.bind(this);
     }
+    componentDidMount() {
+        if (!this.state.set) console.log(this.props.query);
+        let type = {};
+        this.props.query?.type?.forEach((x) => (type[x] = true));
+        this.setState({
+            query: {
+                ...this.state.query,
+                minPrice: this.props.query?.minPrice,
+                maxPrice: this.props.query?.maxPrice,
+                startDate: this.props.query?.startDate,
+                lastDate: this.props.query?.lastDate,
+                city: this.props.query?.location?.city,
+                state: this.props.query?.location?.state,
+                country: this.props.query?.location?.country,
+                type,
+            },
+        });
+    }
+    componentWillReceiveProps() {
+        let type = {};
+        this.props.query?.type?.forEach((x) => (type[x] = true));
+        this.setState({
+            query: {
+                ...this.state.query,
+                minPrice: this.props.query?.minPrice,
+                maxPrice: this.props.query?.maxPrice,
+                startDate: this.props.query?.startDate,
+                lastDate: this.props.query?.lastDate,
+                city: this.props.query?.location?.city,
+                state: this.props.query?.location?.state,
+                country: this.props.query?.location?.country,
+                type,
+            },
+        });
+    }
     toggleHeaders(name) {
-        console.log(name);
-        console.log(this.state.headers);
         this.setState({
             headers: {
                 ...this.state.headers,
@@ -33,16 +69,53 @@ export default class QueryInput extends Component {
     }
     handleChange(e) {
         this.setState({
-            query: {
-                ...this.state.query,
-                [e.target.name]: e.target.value,
-            },
+            query:
+                e.target.id === ""
+                    ? {
+                          ...this.state.query,
+                          [e.target.name]: e.target.value,
+                          set: true,
+                      }
+                    : {
+                          ...this.state.query,
+                          type: {
+                              ...this.state.query.type,
+                              [e.target.id]: e.target.checked,
+                              set: true,
+                          },
+                      },
         });
+
         console.log(this.state.query);
     }
-    submitQuery(e) {
+    async submitQuery(e) {
         e.preventDefault();
-        console.log(this.state.query);
+        let query = {};
+        query.minPrice = +this.state.query.minPrice;
+        query.maxPrice = +this.state.query.maxPrice;
+        query.startDate = this.state.query.startDate;
+        query.lastDate = this.state.query.lastDate;
+        query.location = {};
+        if (this.state.query.city) query.location.city = this.state.query.city;
+        if (this.state.query.state)
+            query.location.state = this.state.query.state;
+        if (this.state.query.country)
+            query.location.country = this.state.query.country;
+        query.type = [];
+        for (let field in this.state.query.type) {
+            if (this.state.query.type[field]) query.type.push(field);
+        }
+        for (let field in query) {
+            !query[field] && delete query[field];
+        }
+        console.log(query);
+        await this.props.setQuery(query);
+        this.props.history.push({
+            pathname: `/jobs/`,
+            state: {
+                query: query,
+            },
+        });
     }
     render() {
         return (
@@ -70,31 +143,61 @@ export default class QueryInput extends Component {
                                 <CustomInput
                                     type='checkbox'
                                     id='cleaning'
+                                    name='cleaning'
+                                    onChange={this.handleChange}
                                     label='Cleaning'
+                                    defaultChecked={
+                                        this.state.query.type.cleaning
+                                    }
                                 />
                                 <CustomInput
+                                    onChange={this.handleChange}
                                     type='checkbox'
                                     id='carpeter'
+                                    name='carpeter'
+                                    defaultChecked={
+                                        this.state.query.type.carpenter
+                                    }
                                     label='Carpeter'
                                 />
                                 <CustomInput
+                                    onChange={this.handleChange}
                                     type='checkbox'
                                     id='plumber'
+                                    defaultChecked={
+                                        this.state.query.type.plumber
+                                    }
+                                    name='plumber'
                                     label='Plumber'
                                 />
                                 <CustomInput
+                                    onChange={this.handleChange}
                                     type='checkbox'
                                     id='appliance'
+                                    name='appliance'
+                                    defaultChecked={
+                                        this.state.query.type.appliance
+                                    }
                                     label='Appliance Repair'
                                 />
                                 <CustomInput
+                                    onChange={this.handleChange}
                                     type='checkbox'
                                     id='manual'
+                                    name='manual'
+                                    defaultChecked={
+                                        this.state.query.type.manual
+                                    }
                                     label='Manual Labor'
                                 />
                                 <CustomInput
+                                    onChange={this.handleChange}
                                     type='checkbox'
                                     id='painter'
+                                    name='painter'
+                                    defaultChecked={
+                                        this.state.query.type.painter
+                                    }
                                     label='Painter'
                                 />
                             </>
@@ -126,6 +229,7 @@ export default class QueryInput extends Component {
                                         size='sm'
                                         name='city'
                                         placeholder='City'
+                                        value={this.state.query.city}
                                         onChange={this.handleChange}
                                     />
                                 </div>
@@ -135,6 +239,7 @@ export default class QueryInput extends Component {
                                         size='sm'
                                         name='state'
                                         onChange={this.handleChange}
+                                        value={this.state.query.state}
                                         placeholder='State'
                                     />
                                 </div>
@@ -144,6 +249,7 @@ export default class QueryInput extends Component {
                                         size='sm'
                                         name='country'
                                         onChange={this.handleChange}
+                                        value={this.state.query.country}
                                         placeholder='Country'
                                     />
                                 </div>
@@ -171,24 +277,83 @@ export default class QueryInput extends Component {
                         </h6>
                         {this.state.headers.time && (
                             <>
-                                <Label for='startDate'>Start Date</Label>
+                                <Label for='startDate'>From</Label>
                                 <Input
-                                    type='date'
+                                    type='datetime-local'
                                     size='sm'
                                     name='startDate'
-                                    id='startDate'
+                                    value={this.state.query.startDate}
                                     onChange={this.handleChange}
                                     placeholder='date placeholder'
                                 />
-                                <Label for='lastDate'>Last Date</Label>
+                                <Label for='lastDate'>To</Label>
                                 <Input
-                                    type='date'
+                                    type='datetime-local'
                                     size='sm'
                                     name='lastDate'
-                                    id='lastDate'
                                     onChange={this.handleChange}
+                                    value={this.state.query.lastDate}
                                     placeholder='date placeholder'
                                 />
+                            </>
+                        )}
+                    </FormGroup>
+                    <hr />
+
+                    <FormGroup>
+                        <h6>
+                            Price
+                            <div
+                                name='price'
+                                className='float-right'
+                                onClick={(e) => this.toggleHeaders("price")}>
+                                <FontAwesomeIcon
+                                    icon={
+                                        this.state.headers.price
+                                            ? faChevronUp
+                                            : faChevronDown
+                                    }
+                                />
+                            </div>
+                        </h6>
+                        {this.state.headers.price && (
+                            <>
+                                <Label>
+                                    Min Price - ₹
+                                    {this.state.query.minPrice
+                                        ? this.state.query.minPrice
+                                        : 0}
+                                </Label>
+                                <div className='d-flex flex-row'>
+                                    <span>₹0</span>
+                                    <CustomInput
+                                        type='range'
+                                        className='mx-1'
+                                        name='minPrice'
+                                        min='0'
+                                        onChange={this.handleChange}
+                                        max='10000'
+                                        value={this.state.query.minPrice}
+                                    />
+                                    <span>₹10000</span>
+                                </div>
+
+                                <Label>
+                                    Max Price - ₹{this.state.query.maxPrice}
+                                </Label>
+                                <div className='d-flex flex-row'>
+                                    <span>₹0</span>
+                                    <CustomInput
+                                        type='range'
+                                        className='mx-1'
+                                        name='maxPrice'
+                                        min='0'
+                                        value={this.state.query.maxPrice}
+                                        onChange={this.handleChange}
+                                        max='10000'
+                                    />
+                                    <span>₹10000</span>
+                                </div>
                             </>
                         )}
                     </FormGroup>
