5ee97f43927fd45b5f27b698d9bf0a07a4d2a80d
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-22 01:25:43 +0530

    add jobcard component

---
+++ client/src/components/jobCard.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, CardTitle, CardText } from "reactstrap";
+
+const JobCard = ({ currentJob, job, setCurrentJob }) => {
+    const days = parseInt(
+        (new Date() - new Date(job.updatedAt)) / (1000 * 60 * 60 * 24),
+        10,
+    );
+    return (
+        <div
+            className='col-12 p-2'
+            onClick={() =>
+                setCurrentJob(
+                    currentJob && job._id === currentJob._id ? null : job,
+                )
+            }>
+            <Card className='row flex-row h-100'>
+                <div className='col-12 p-3'>
+                    <CardTitle tag='h5'>{job.title}</CardTitle>
+                    <CardText>{job.description.slice(0, 100)} ...</CardText>
+                    <CardText>
+                        {`${job.location.city}, ${job.location.state}, ${job.location.country}`}
+                        <br />
+                        <small className='text-muted'>
+                            Last updated{" "}
+                            {days === 0 ? "today" : `${days} days ago`}
+                        </small>
+                    </CardText>
+                </div>
+            </Card>
+        </div>
+    );
+};
+export default JobCard;
