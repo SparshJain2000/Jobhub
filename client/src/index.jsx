836aeb20910eacfff4b27587cba18cc8f58c2b5b
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-23 01:26:03 +0530

    connect to apollo client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./bootstrap.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const client = new ApolloClient({
+    uri: "http://localhost:3000/graphql",
+    cache: new InMemoryCache(),
+});
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById("root"),
 );
