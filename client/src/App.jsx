df490ada1291a11975e2b6b711db03f6121409df
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-24 00:54:13 +0530

    remove unnecessary comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,6 @@ function App() {
                 <Switch>
                     <Route path='/' exact component={Home} />
                     <Route path='/jobs' exact component={Job} />
-                    {/* <Route path='/about' component={About} />
-                    <Route path='/event' exact component={Event} />
-                    <Route path='/event/national' component={NationalEvent} />
-                    <Route path='/event/workshop' component={WorkshopEvent} />
-                    <Route path='/event/inhouse' component={InhouseEvent} />
-                    <Route path='/sponsors' exact component={Sponsor} />
-                    <Route path='/sponsors/become' component={BecomeSponsor} /> */}
                 </Switch>
             </main>
         </BrowserRouter>
