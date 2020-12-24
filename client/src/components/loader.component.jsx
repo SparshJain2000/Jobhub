c75273d3cb2363324aa017838d2dad60b3229d96
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-24 22:47:24 +0530

    create Loader

---
+++ client/src/components/loader.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/loader.css";
+const Loader = () => (
+    <div className='sk-chase mx-auto mt-40vh'>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+    </div>
+);
+
+export default Loader;
