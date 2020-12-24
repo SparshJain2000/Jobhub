4d9d65a99c6dd2a3747b00f086117942265a8212
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-25 01:36:51 +0530

    job category cards

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -2,33 +2,70 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import img from "../assets/home.png";
 import AnimatedBackground from "../components/animatedBackground.component";
-
+import { types, images } from "../assets/data";
 import "../styles/home.css";
 const Home = () => {
     return (
         <>
-            <AnimatedBackground />
-            <div className='row mx-0'>
-                <div className='col-12 col-md-6 my-auto pl-5 '>
-                    <h1 className='text-white'>JobHub </h1>
-                    <p style={{ color: "var(--gray)" }}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Labore illum sint eligendi at, ipsam ipsum non, iste, et
-                        eos
-                    </p>
-                    <div>
-                        <button className='btn btn-sm btn-secondary mr-1'>
-                            Hire worker
-                        </button>
-                        <Link
-                            to='/jobs'
-                            className='btn btn-sm btn-outline-secondary ml-1'>
-                            Get Jobs
-                        </Link>
+            <div className='position-relative'>
+                <AnimatedBackground />
+                <div className='row mx-0'>
+                    <div className='col-12 col-md-6 my-auto pl-5 '>
+                        <h1 className='text-white'>JobHub </h1>
+                        <p style={{ color: "var(--gray)" }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Labore illum sint eligendi at, ipsam ipsum
+                            non, iste, et eos
+                        </p>
+                        <div>
+                            <button className='btn btn-sm btn-secondary mr-1'>
+                                Hire worker
+                            </button>
+                            <Link
+                                to='/jobs'
+                                className='btn btn-sm btn-outline-secondary ml-1'>
+                                Get Jobs
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 d-none d-md-flex'>
+                        <img src={img} className='img-fluid' alt='photu' />
                     </div>
                 </div>
-                <div className='col-12 col-md-6 d-none d-md-flex'>
-                    <img src={img} className='img-fluid' alt='photu' />
+                <div className=' mt-auto-bg '>
+                    <h3 className='col-12 '>Jobs By Category</h3>
+                    <hr className='col-10 col-md-3 header-line' />
+                    <div className='home-types row'>
+                        {types.map((type) => (
+                            <div
+                                className='col-6 col-sm-4 col-md-2 p-2'
+                                style={{ height: "100%" }}>
+                                <Link
+                                    className='type-link'
+                                    to={{
+                                        pathname: "/jobs/",
+                                        state: {
+                                            query: {
+                                                type: [
+                                                    type
+                                                        .split(" ")[0]
+                                                        .toLowerCase(),
+                                                ],
+                                            },
+                                        },
+                                    }}>
+                                    <div className='py-3 px-4 type-card text-align-center'>
+                                        <h6 className='border-b'>{type}</h6>
+                                        <img
+                                            src={images[type]}
+                                            alt=''
+                                            className='img-fluid '
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
