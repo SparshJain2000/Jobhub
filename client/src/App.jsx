9b233f6fad1df8147a78895c7a6f487306cea922
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-25 00:07:41 +0530

    Add loader and about route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,34 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home.page";
 import Job from "./pages/job.page";
+import About from "./pages/about.page";
 import Navbar from "./components/navbar.component";
+import Loading from "./components/loader.component";
+import { Component } from "react";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Navbar />
-            <main>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/jobs' exact component={Job} />
-                </Switch>
-            </main>
-        </BrowserRouter>
-    );
+class App extends Component {
+    state = { loading: true };
+    componentDidMount() {
+        this.setState({ loading: false });
+    }
+    render() {
+        return (
+            <BrowserRouter>
+                <Navbar />
+                {this.state.loading ? (
+                    <Loading />
+                ) : (
+                    <main>
+                        <Switch>
+                            <Route path='/' exact component={Home} />
+                            <Route path='/jobs' exact component={Job} />
+                            <Route path='/about' exact component={About} />
+                        </Switch>
+                    </main>
+                )}
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
