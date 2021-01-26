58708bbc49ebbdc117d3b616cbcd137f4c3dfc8c
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-26 23:54:35 +0530

    employer model : change contact type to string

--- models/employer.model.js
+++ models/employer.model.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
-const employerSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    contact: {
-        type: Number,
-        required: true,
-        min: 6000000000,
-        max: 9999999999,
+const employerSchema = new mongoose.Schema(
+    {
+        firstName: String,
+        lastName: String,
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        contact: {
+            type: String,
+            required: true,
+            min: 6000000000,
+            max: 9999999999,
+        },
+        jobs: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Job",
+            },
+        ],
     },
-    jobs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Job",
-    }, ],
-}, { timestamps: true }, );
+    { timestamps: true },
+);
 
 module.exports = mongoose.model("Employer", employerSchema);
