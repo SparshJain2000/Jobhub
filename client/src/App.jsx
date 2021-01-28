aea78dd5c0c5659b28ba144a7556c57af98596cf
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-29 00:30:21 +0530

    create auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,85 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/home.page";
 import Job from "./pages/job.page";
+import EmployerLogin from "./pages/employerLogin.page";
+import EmployerSignup from "./pages/employerSignup.page";
 import About from "./pages/about.page";
 import Navbar from "./components/navbar.component";
 import Loading from "./components/loader.component";
 import { Component } from "react";
-
+import AuthContext from "./context/auth.context";
 class App extends Component {
-    state = { loading: true };
+    state = { loading: true, token: null, userId: null, isEmployer: false };
     componentDidMount() {
         this.setState({ loading: false });
+        if (sessionStorage.getItem("user")) {
+            const user = JSON.parse(sessionStorage.getItem("user"));
+            console.log(user);
+            console.log(new Date(user.expiry) > new Date());
+            if (new Date(user.expiry) < new Date()) {
+                console.log("EXPIRED !!!");
+                this.logout();
+            } else this.setState({ loading: false, ...user });
+        } else {
+            const { loading, ...user } = this.state;
+            sessionStorage.setItem("user", JSON.stringify(user));
+        }
     }
+    login = (token, userId, tokenExpiration, isEmployer) => {
+        this.setState({ token, userId, isEmployer });
+        sessionStorage.setItem(
+            "user",
+            JSON.stringify({
+                token,
+                userId,
+                isEmployer,
+                expiry: new Date(
+                    new Date().getTime() + 60000 * 60 * tokenExpiration,
+                ),
+            }),
+        );
+    };
+    logout = () => {
+        this.setState({ token: null, userId: null, isEmployer: false });
+        sessionStorage.removeItem("user");
+    };
     render() {
         return (
             <BrowserRouter>
-                <Navbar />
-                {this.state.loading ? (
-                    <Loading />
-                ) : (
-                    <main>
-                        <Switch>
-                            <Route path='/' exact component={Home} />
-                            <Route path='/jobs' exact component={Job} />
-                            <Route path='/about' exact component={About} />
-                        </Switch>
-                    </main>
-                )}
+                <AuthContext.Provider
+                    value={{
+                        token: this.state.token,
+                        userId: this.state.userId,
+                        isEmployer: this.state.isEmployer,
+                        login: this.login,
+                        logout: this.logout,
+                    }}>
+                    <Navbar />
+                    {this.state.loading ? (
+                        <Loading />
+                    ) : (
+                        <main>
+                            <Switch>
+                                <Route path='/' exact component={Home} />
+                                <Route path='/jobs' exact component={Job} />
+                                <Route path='/about' exact component={About} />
+                                {this.state.token && (
+                                    <Redirect from='/employer' to='/' />
+                                )}
+                                <Route
+                                    path='/employer/login'
+                                    exact
+                                    component={EmployerLogin}
+                                />
+                                <Route
+                                    path='/employer/signup'
+                                    exact
+                                    component={EmployerSignup}
+                                />
+                            </Switch>
+                        </main>
+                    )}
+                </AuthContext.Provider>
             </BrowserRouter>
         );
     }
