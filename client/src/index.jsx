169e688a4d2279491c4af9631f174627e22febd2
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-29 01:28:56 +0530

    create middleware(authorization token)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,11 +3,33 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/style.css";
 import "./bootstrap.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    HttpLink,
+    ApolloLink,
+    concat,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({ uri: "http://localhost:3000/graphql" });
+const authMiddleware = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+    console.log("inide");
+    const token = JSON.parse(sessionStorage.getItem("user"))?.token;
+
+    operation.setContext({
+        headers: {
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    });
+
+    return forward(operation);
+});
 
 const client = new ApolloClient({
-    uri: "http://localhost:3000/graphql",
     cache: new InMemoryCache(),
+    link: concat(authMiddleware, httpLink),
 });
 ReactDOM.render(
     <React.Fragment>
