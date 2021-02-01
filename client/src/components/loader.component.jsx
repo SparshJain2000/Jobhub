f49b817f40ce6ed3dfb72ad99c6470f41ec3cb26
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-02 00:35:50 +0530

    UI changes

--- client/src/components/loader.component.jsx
+++ client/src/components/loader.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Loader = () => (
-    <div className='sk-chase mx-auto mt-40vh'>
+    <div className='sk-chase mx-auto my-30vh'>
         <div className='sk-chase-dot'></div>
         <div className='sk-chase-dot'></div>
         <div className='sk-chase-dot'></div>
