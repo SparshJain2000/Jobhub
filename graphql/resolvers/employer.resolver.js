26fcf2480a99f4e66af944097236c26e707a5aa9
SparshJain2000 <jainsparsh0801@gmail.com>
2021-03-18 00:27:27 +0530

    create currentrEmployer resolver

--- graphql/resolvers/employer.resolver.js
+++ graphql/resolvers/employer.resolver.js
@@ -3,10 +3,20 @@ const { populateJobs } = require("../helpers/index");
 const { projectionBuilder } = require("../../controller/queryBuilder");
 
 module.exports = {
-    employers: async(_, args, context) => {
+    employers: async (_, args, context) => {
         const projection = projectionBuilder(context);
         let x = await Employer.find({}, projection).lean();
         x = projection.jobs ? x.map((employer) => populateJobs(employer)) : x;
         return x;
+    },
+    currentEmployer: async (_, req, context) => {
+        if (!req.isAuth) throw Error("UNAUTHENTICATED");
+        if (!req.isEmployer) throw Error("NOT_EMPLOYER");
+        const projection = projectionBuilder(context);
+        console.log(projection);
+        let emp = await Employer.findById(req.userId, projection).lean();
+        emp = projection.jobs ? await populateJobs(emp) : emp;
+        console.log(emp);
+        return emp;
     },
 };
